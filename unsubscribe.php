b72d6d806536af29f5cc516470db54af949be4d1
prashantsinalkar <prashantsinalkar@gmail.com>
2016-01-14 14:25:12 +0530

    changes done in according to multiple events

--- unsubscribe.php
+++ unsubscribe.php
@@ -1,60 +1,93 @@
 <?php
-
-require_once('connection.inc');
-header('Refresh: 3; URL=http://scipy.in');
-if(isset($_GET) && isset($_GET['key'])){
-
-	 unsubscribeEmailid($_GET['key']);
-
-}else{
-
-echo 'wrong link';
-}
-
-function unsubscribeEmailid($email_id_hash)
+  require_once('connection.inc');
+  if (isset($_GET) && isset($_GET['key']))
     {
-    global $conn;
-
-   // try to update user with specified information
-
-                 $sql = "SELECT email, unsubscribe, email_hash  FROM sent_email WHERE email_hash = :email_id_hash ";
-                 $q = $conn->prepare($sql);
-                 $q->execute(array('email_id_hash'=>$email_id_hash));
-
-
-  while($data = $q->fetchAll(PDO::FETCH_ASSOC)) {
-        foreach($data as $row){
-
-
-            if($data != NULL){
-
-                      if($row['unsubscribe'] == 0 && $row['email_hash'] == $email_id_hash){
-
-			       $sql_up = "UPDATE sent_email SET unsubscribe = 1 WHERE email_hash =:email_id_hash ";
-                               $q_up = $conn->prepare($sql_up);
-                               $q_up->execute(array(':email_id_hash'=>$email_id_hash));
-
-                             echo '<br>Thank You for unsubscription';
-                             echo '<br><br>If you are not automatically redirected, click here: <a href="http://scipy.in">SciPy India 2015</a>.';
-                     }
-
-            elseif($row['unsubscribe'] == 1 && $row['email_hash'] == $email_id_hash){
-                             echo '<br>You are already unsubscribed!';
-                             echo '<br><br>If you are not automatically redirected, click here: <a href="http://scipy.in">SciPy India 2015</a>.';
-            }
-
-            else{
-                             echo '<br>You are not a subscriber!';
-                             echo '<br><br>If you are not automatically redirected, click here: <a href="http://scipy.in">SciPy India 2015</a>.';
-              }
-
-         }else{
-
-              echo "Wrong Link please try again";  
-      }
+      $email_id_hash = $_GET['key'];
+      $event         = $_GET['event'];
+      unsubscribeEmailid($email_id_hash, $event);
     }
-  }
-}
-
-$conn=null;
+  else
+    {
+      echo 'wrong link';
+    }
+  function unsubscribeEmailid($email_id_hash, $event)
+    {
+      global $conn;
+      // try to update user with specified information
+      $email_id_hash = urldecode($email_id_hash);
+      $sql           = "SELECT email, unsubscribe, email_hash  FROM sent_email WHERE email_hash = :email_id_hash AND event= :event";
+      $q             = $conn->prepare($sql);
+      $q->execute(array(
+          'email_id_hash' => $email_id_hash,
+          'event' => $event
+      ));
+      while ($data = $q->fetchAll(PDO::FETCH_ASSOC))
+        {
+          foreach ($data as $row)
+            {
+              if ($data != NULL)
+                {
+                  switch ($event)
+                  {
+                      case 'scipy 2015':
+                          if ($row['unsubscribe'] == 0 && $row['email_hash'] == $email_id_hash)
+                            {
+                              $sql_up = "UPDATE sent_email SET unsubscribe = 1 WHERE email_hash =:email_id_hash ";
+                              $q_up   = $conn->prepare($sql_up);
+                              $q_up->execute(array(
+                                  ':email_id_hash' => $email_id_hash
+                              ));
+                              echo '<br>Thank You for unsubscription';
+                              echo '<br><br>If you are not automatically redirected, click here: <a href="http://scipy.in">SciPy India 2015</a>.';
+                              header('Refresh: 3; URL=http://scipy.in');
+                            }
+                          elseif ($row['unsubscribe'] == 1 && $row['email_hash'] == $email_id_hash)
+                            {
+                              echo '<br>You are already unsubscribed!';
+                              echo '<br><br>If you are not automatically redirected, click here: <a href="http://scipy.in">SciPy India 2015</a>.';
+                              header('Refresh: 3; URL=http://scipy.in');
+                            }
+                          else
+                            {
+                              echo '<br>You are not a subscriber!';
+                              echo '<br><br>If you are not automatically redirected, click here: <a href="http://scipy.in">SciPy India 2015</a>.';
+                              header('Refresh: 3; URL=http://scipy.in');
+                            }
+                          break;
+                      case 'cfd-openfoam-symposium 2016':
+                          if ($row['unsubscribe'] == 0 && $row['email_hash'] == $email_id_hash)
+                            {
+                              $sql_up = "UPDATE sent_email SET unsubscribe = 1 WHERE email_hash =:email_id_hash";
+                              $q_up   = $conn->prepare($sql_up);
+                              $q_up->execute(array(
+                                  ':email_id_hash' => $email_id_hash
+                              ));
+                              echo '<br>Thank You for unsubscription';
+                              echo '<br><br>If you are not automatically redirected, click here: <a href="http://fossee.in/conference/cfd-symposium/">OpenFOAM Symposium 2016</a>.';
+                              header('Refresh: 3; URL=http://fossee.in/conference/cfd-symposium/');
+                            }
+                          elseif ($row['unsubscribe'] == 1 && $row['email_hash'] == $email_id_hash)
+                            {
+                              echo '<br>You are already unsubscribed!';
+                              echo '<br><br>If you are not automatically redirected, click here: <a href="http://fossee.in/conference/cfd-symposium/">OpenFOAM Symposium 2016</a>.';
+                              header('Refresh: 3; URL=http://fossee.in/conference/cfd-symposium/');
+                            }
+                          else
+                            {
+                              echo '<br>You are not a subscriber!';
+                              echo '<br><br>If you are not automatically redirected, click here: <a href="http://scipy.in">OpenFOAM Symposium 2016</a>.';
+                              header('Refresh: 3; URL=http://fossee.in/conference/cfd-symposium/');
+                            }
+                          break;
+                  }
+                }
+              else
+                {
+                  echo "Wrong Link please try again";
+                  header('Refresh: 3; URL=http://fossee.in');
+                }
+            }
+        }
+    }
+  $conn = null;
 ?>
